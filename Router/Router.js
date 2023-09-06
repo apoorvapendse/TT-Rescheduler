070f61e37f776b7bc302de83e4cb3daaba506d61
shreyasruikar <139792211+shreyasruikar@users.noreply.github.com>
2023-09-06 19:24:50 +0530

    Added Router for signup page

--- Router/Router.js
+++ Router/Router.js
@@ -1,13 +1,43 @@
 import express from "express";
 import { home, loginPage, auth } from "../Controllers/home.js";
+import { UserModel } from "../database/database.js";
 const router = express.Router();
 
 router.get("/", home);
+router.get("/login", loginPage);
+router.post("/login", auth);
 
-router.get("/login", loginPage)
+//Adding new Route for login
 
-router.post("/login", auth)
 
+
+//Adding new Route for Sign-up
+
+router.post("/signup", async (req, res) => {
+    try {
+    const{ firstname, lastname, email, password } = req.body;
+
+    const existingUser = await UserModel.findone({email});
+
+    if(existingUser) {
+        return res.status(400).json({message: "User Already Exists." });
+    }
+
+    const newUser = new UserModel({
+        email,
+        password,
+        firstname,
+        lastname,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully." });
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
 
 export default router
 
