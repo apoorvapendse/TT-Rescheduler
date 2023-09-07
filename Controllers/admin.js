fdce267f2442de165c067fb029f742f52ee7896c
Rajeev Tapadia <rtapadia730@gmail.com>
2023-09-07 23:01:41 +0530

    faculty creation done

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -1,12 +1,38 @@
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import Professors from "../models/Faculty.js"
+
 const adminDashGet = (req, res) => {
-    res.render('adminDash.ejs')
+  res.render('adminDash.ejs')
 }
 
-const createFacultyPost = (req, res) => {
-    
+const createFacultyPost = async (req, res) => {
+  console.log("Create faculty post called")
+  const checkProf = await Professors.findOne({ email: req.body.email })
+  if (checkProf)
+    res.json("prof acc already exists")
+  else {
+    const newProf = new Professors({
+      UID: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10)
+    });
+
+    newProf
+      .save()
+      .then(result => {
+        console.log("prof created:", result)
+        res.json("prof created")
+      })
+      .catch(err => {
+        console.log(err)
+        req.json("could not create prof acc")
+      })
+  }
 }
 
 export {
-    adminDashGet,
-    createFacultyPost
+  adminDashGet,
+  createFacultyPost
 }
