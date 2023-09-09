208007c086a3e0722919dd8792eb741ecd35299b
Rajeev Tapadia <rtapadia730@gmail.com>
2023-09-10 01:10:24 +0530

    forgot git add .

--- Controllers/api.js
+++ Controllers/api.js
@@ -1,5 +1,8 @@
-const getFaculty = (req, res) => {
+import Professors from '../models/Faculty.js'
 
+const getFaculty = async (req, res) => {
+  const facultyList = await Professors.find({})
+  res.status(200).send(facultyList)
 }
 
 export { getFaculty }
