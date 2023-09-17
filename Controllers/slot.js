6d78686b95137895b7a82ff8f91ce0b15a34dede
apoorvapendse <apoorvavpendse@gmail.com>
2023-09-17 23:58:14 +0530

    pushes in sentreqests array for sender

--- Controllers/slot.js
+++ Controllers/slot.js
@@ -1,10 +1,28 @@
+import jwtDecode from "jwt-decode";
+import Professors from "../models/Faculty.js";
+
 async function getRequestSlot(req, res) {
   console.log("slot is requested");
   res.render("reqslot.ejs");
 }
 async function postRequestSlot(req, res) {
   console.log(req.body);
-  res.json(req.body);
+  const profToken = req.cookies.proftoken;
+  const senderProfID = jwtDecode(profToken).id;
+
+  const senderProf = await Professors.findById(senderProfID);
+
+  if (senderProf) {
+    senderProf.sentRequests.push({
+      time: req.body.time,
+      day: req.body.day,
+      approved: false,
+    });
+
+    senderProf.save();
+    res.send("<h1>Request sent successfully</h1>");
+  }
+  res.json("something went wrong");
 }
 
 export { getRequestSlot, postRequestSlot };
--- models/Faculty.js
+++ models/Faculty.js
@@ -2,13 +2,26 @@ import mongoose, { Schema } from "mongoose";
 import timeTables from "./TimeTable.js";
 import { LectureSchema, timeTableSchema } from "./schema.js";
 
+const sentReqSchema = new mongoose.Schema({
+  time: Number,
+  day: String,
+  approved: Boolean,
+});
+
+const receivedReqSchema = new mongoose.Schema({
+  time: Number,
+  day: String,
+  approved: Boolean,
+});
 
 const facultySchema = new mongoose.Schema({
   UID: String,
   name: String,
   email: String,
   password: String,
-  tt: { type: Schema.Types.ObjectId, ref: 'TimeTables' }
+  sentRequests: [sentReqSchema],
+  receivedReqSchema: [receivedReqSchema],
+  tt: { type: Schema.Types.ObjectId, ref: "TimeTables" },
 });
 
 const Professors = mongoose.model("Professors", facultySchema);
