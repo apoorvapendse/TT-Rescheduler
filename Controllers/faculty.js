edb887fc24513aed618546aae3d207321af89198
Rajeev Tapadia <rtapadia730@gmail.com>
2023-09-29 14:32:40 +0530

    fixed req list depending on profID in localStorage

--- Controllers/faculty.js
+++ Controllers/faculty.js
@@ -1,9 +1,21 @@
+import jwtDecode from "jwt-decode";
+
+
 const profDashGet = (req, res) => {
   res.status(200).render("prof_dashboard.ejs");
 };
 
 const profDashPost = (req, res) => {
   console.log(req.body);
+  try{
+    const proftoken = jwtDecode(req.cookies.proftoken)
+    
+  }catch(err){console.log(err);}
+  if(proftoken){
+    console.log(proftoken);
+
+  }
+
   res.status(200).json("bimbimbambam");
 };
 
