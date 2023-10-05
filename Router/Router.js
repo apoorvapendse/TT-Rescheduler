66ef0b81985f30751cf97f1ef3ad235b2ac2205d
shreyasruikar <139792211+shreyasruikar@users.noreply.github.com>
2023-10-05 22:36:28 +0530

    reqslot styling

--- Router/Router.js
+++ Router/Router.js
@@ -1,48 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
-import { home, loginPage, auth } from "../Controllers/home.js";
-import { UserModel } from "../database/database.js";
-const router = express.Router();
-
-router.get("/", home);
-router.get("/login", loginPage);
-router.post("/login", auth);
-
-//Adding new Route for login
-
-
-
-//Adding new Route for Sign-up
-
-router.post("/signup", async (req, res) => {
-    try {
-    const{ firstname, lastname, email, password } = req.body;
-
-    const existingUser = await UserModel.findone({email});
-
-    if(existingUser) {
-        return res.status(400).json({message: "User Already Exists." });
-    }
-
-    const newUser = new UserModel({
-        email,
-        password,
-        firstname,
-        lastname,
-    });
-
-    await newUser.save();
-
-    res.status(201).json({ message: "User created successfully." });
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({ message: "Server Error" });
-    }
-});
-
-export default router
-
-=======
 import * as auth_control from "../Controllers/auth.js";
 import * as admin_control from "../Controllers/admin.js";
 import { checkAdmin, checkProf } from "../Controllers/authMiddleware.js";
@@ -95,4 +51,4 @@ router.get("/faculty/request-slot", checkProf, getRequestSlot);
 router.post("/faculty/request-slot", checkProf, postRequestSlot);
 
 export default router;
->>>>>>> a24b102ef3c67456a20f70d4d755d7f04edebad8
+
