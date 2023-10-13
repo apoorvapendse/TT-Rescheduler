f85f94b17659a52479b31b4fa0ab856f958de3be
9ifrashaikh <139782773+9ifrashaikh@users.noreply.github.com>
2023-10-13 19:29:28 +0530

    fixed things on admin dash

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ app.use(cookieParser());
 //use router
 app.use("/", router);
 
+// route for rendering "index.ejs"
+
+app.get("/index", (req, res) => {
+  res.render("index"); 
+});
+
+app.get("/About", (req, res) => {
+  res.render("About"); 
+});
+
+
 // const mongoURI = process.env.MONGO_URI;
 // const port = process.env.PORT;
 // const jwtPassword = process.env.JWT_PASSWD;
